6b288fd9c27f64a33c43c4418a4fa82cdcc487a4
bbahna <moondomi_@naver.com>
2022-02-02 16:56:23 +0900

    start typescript-react

---
+++ README.md
@@ -0,0 +1,10 @@
+# TIL) typescript-react learned recode
+ - 2022.1.27 start ~ end_of_February Plan
+
+**Code/Readme GitHub commit**
+1. 기본설정(CRA*/Ts*/Sass*/Template*/Route* 등)
+2. CSS(반응형/grid,flex/scss* 등)
+3. 추가응용(Modal*, PopUp*)
+4. Library 활용(다국어*/material-ui/Swiper/Hamburger/Carousel/ScrollSpy)
+5. 모던React(useState/styled-components 등)
++ Gatsby 1인 프로젝트(2주)
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App-wrap'>
+      <header className='App-header'>
+        <h1>Lifelog</h1>
+        <p>효동동 Blog</p>
+      </header>
+      <body className='App-body'>
+        <section className='blog'>
+          Node.js를 활용한 게시판 구현(예정)
+        </section>
+      </body>
+      <footer className='App-footer'>
+        ((이곳은 푸터영역))
+      </footer>
+    </div>
+  );
+}
+
+export default App;
