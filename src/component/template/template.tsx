285f55ce4f323d897719cbc225fcb0d4ae8f1279
bbahna <moondomi_@naver.com>
2022-02-12 21:17:16 +0900

    Link 설정

--- src/component/template/template.tsx
+++ src/component/template/template.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './template.scss';
+import { Link } from 'react-router-dom';
 
-interface IProps extends React.Props<{}> {
+interface IProps {
+  children?: React.ReactElement;
 }
 
 const Template = ({children}: IProps) => {
@@ -12,6 +14,16 @@ const Template = ({children}: IProps) => {
           <h1>Lifelog</h1>
           <p>효동동 Blog</p>
         </div>
+        <nav>
+          <ul className='navi-menu'>
+            <li>
+              <Link to="/">Main</Link>
+            </li>
+            <li>
+              <Link to="/sub">Sub</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
       {children}
       <footer>
