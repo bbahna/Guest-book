5645cb8e68030687a8310ae0c55867ccaaac9cd7
bbahna <moondomi_@naver.com>
2022-03-09 17:08:06 +0900

    Popup page style 수정

--- src/pages/popup/popup.tsx
+++ src/pages/popup/popup.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import './popup.scss';
-import Template from '../../component/template/template';
 
 function PopupPage() {
   return (
     <div id='popup-wrap'>
-      <Template>
-        <body>
-          <section>
-            * 팝업 페이지<br/>
-            - onClick시, PopUp open
-          </section>
-        </body>
-      </Template>
+      <body>
+        <h2>* 팝업 페이지</h2>
+        <p>- onClick시, PopUp open</p>
+      </body>
     </div>
   );
 }
